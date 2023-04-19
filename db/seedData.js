a3a861cf4fe961e3f47a3221691b443392c7f699
Tippy <Pixie2shot@gmail.com>
2023-04-19 13:20:07 -0500

    create users file

--- db/seedData.js
+++ db/seedData.js
@@ -1,4 +1,5 @@
 const client = require('./client');
+const { createUser } = require('./users');
 
 const dropTables = async () => {
   try {
@@ -10,7 +11,7 @@ const dropTables = async () => {
   DROP TABLE IF EXISTS users;
   `); console.log("FINISHED DROPPING TABLES")
   } catch (err) {
-    throw err
+    throw err;
   }
 }
 
@@ -57,17 +58,49 @@ const createTables = async () => {
   }
 };
 
+const createInitialUsers = async () => {
+  console.log("STARTING TO CREATE USERS...")
+  try {
+    const usersToCreate = [
+      { username: "mike", password: "mike22"},
+      { username: "alice", password: "alicat7"},
+      { username: "bob", password: "bobert007"},
+    ]
+    const users = await Promise.all(usersToCreate.map(createUser))
+
+    console.log("Users created:")
+    console.log(users)
+    console.log("Finished creating users!")
+  } catch(err) {
+    console.error("ERROR CREATING USERS!")
+    throw err;
+  }
+}
+
 const rebuildTables = async() =>{
 	console.log("DROPPING TABLES");
 	await dropTables();
 	console.log('FINISHED DROPPING TABLES');
 	console.log('BUILDING TABLES');
 	await createTables();
-	console.log('FINISHED BUILDING TABLES')
+	console.log('FINISHED BUILDING TABLES');
 };
+
+const rebuildDb = async() =>{
+  await createInitialUsers();
+}
+
+const testDb = async () =>{
+  console.log("CONNECTING TO DB...");
+  client.connect();
+  console.log("FINISHED CONNECTING");
+};
+
+testDb();
 
 module.exports = {
   rebuildTables,
   dropTables,
-  createTables
+  createTables,
+  rebuildDb
 }
